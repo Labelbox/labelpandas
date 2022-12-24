eb1abc96cb17ec34b2735ddf934ea70e26191ac5
raphaeljafriLB <79109817+raphaeljafriLB@users.noreply.github.com>
2022-12-23 20:22:22 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
   name='labelpandas',
-  version='0.0.65',
+  version='0.0.66',
   packages=setuptools.find_packages(),
   url='https://github.com/Labelbox/labelpandas',
   description='Labelbox Connector for Pandas',
