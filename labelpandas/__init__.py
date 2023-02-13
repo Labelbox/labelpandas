2504ae916b20e7217cfdb9bb1f75a2e1123a6b6e
raphaeljafriLB <79109817+raphaeljafriLB@users.noreply.github.com>
2023-02-13 18:26:36 -0500

    Update __init__.py

--- labelpandas/__init__.py
+++ labelpandas/__init__.py
@@ -1,5 +1,5 @@
 from .client import Client
-from labelpandas.rename_columns import rename_columns
-from .load_local_files import load_local_files
 from labelpandas import connector
 from labelpandas import uploader
+from labelpandas.rename_columns import rename_columns
+from labelpandas.load_local_files import load_local_files
