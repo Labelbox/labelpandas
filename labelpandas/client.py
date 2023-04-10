a5869e5a497827a9b78a41cbebca1b2e25b866d3
raphaeljafriLB <79109817+raphaeljafriLB@users.noreply.github.com>
2023-04-10 23:32:06 +0200

    Update client.py

--- labelpandas/client.py
+++ labelpandas/client.py
@@ -76,16 +76,18 @@ class Client():
             divider             :   Optional (str) - String delimiter for schema name keys and suffix added to duplocate global keys
         """
         # Create/identify the following values:
-            # row_data_col      : column with name "row_data"
-            # global_key_col    : column with name "global_key" - defaults to row_data_col
-            # external_id_col   : column with name "external_id" - defaults to global_key_col
-            # project_id_col    : column with name "project_id" - defaults to ""
-            # dataset_id_col    : column with name "dataset_id" - defaults to ""
-            # external_id_col   : column with name "external_id" - defaults to global_key_col        
-            # metadata_index    : Dictonary where {key=metadata_field_name : value=metadata_type} - defaults to {}
-            # attachment_index  : Dictonary where {key=column_name : value=attachment_type} - defaults to {}
-            # annotation_index  : Dictonary where {key=column_name : value=top_level_feature_name} - defaults to {}
-        row_data_col, global_key_col, external_id_col, project_id_col, dataset_id_col, metadata_index, attachment_index, annotation_index = validate_columns(
+            # row_data_col                :   Column representing asset URL, raw text, or path to local asset file
+            # global_key_col              :   Defaults to row_data_col
+            # external_id_col             :   Defaults to global_key_col
+            # project_id_col              :   Either `project_id` or "" - overridden by arg for project_id        
+            # dataset_id_col              :   Either `dataset_id` or "" - overridden by arg for dataset_id
+            # model_id_col                :   Either `model_id` or "" - overridden by arg for model_id and any args for model_run_id
+            # model_run_id_col            :   Either `model_run_id` or "" - overridden by arg for model_run_id        
+            # metadata_index              :   Dictonary where {key=metadata_field_name : value=metadata_type} or {} if not uploading metadata
+            # attachment_index            :   Dictonary where {key=column_name : value=attachment_type} or {} if not uploading attachments
+            # annotation_index            :   Dictonary where {key=column_name : value=top_level_class_name} or {} if not uploading annotations
+            # prediction_index            :   Dictonary where {key=column_name : value=top_level_class_name} or {}  if not uploading predictions
+        x = validate_uploader_columns(
             client=self.lb_client, table=table,
             get_columns_function=get_col_names,
             get_unique_values_function=get_unique_values,
@@ -94,30 +96,36 @@ class Client():
         
         # Iterating over your pandas DataFrame is faster once converted to a list of dictionaries where {keys=column_names : value=row_column_values}
         table_dict = table.to_dict('records')
+       
         
-        # Determine if we're batching and/or uploading annotations
-        batch_action, annotate_action = determine_actions(
-            dataset_id=dataset_id, dataset_id_col=dataset_id_col, 
-            project_id=project_id, project_id_col=project_id_col, 
-            upload_method=upload_method, annotation_index=annotation_index
+        # Determine the actions we're taking in this run
+        create_action, batch_action, annotate_action, prediction_action = determine_actions(
+            dataset_id=dataset_id, dataset_id_col=x["dataset_id_col"], project_id=project_id, project_id_col=x["project_id_col"], 
+            model_id=model_id, model_id_col=x["model_id_col"], model_run_id=model_run_id, model_run_id_col=x["model_run_id_col"],
+            upload_method=upload_method, annotation_index=x["annotation_index"], prediction_index=x["prediction_index"]
         )
         
-        # Create an upload dictionary where {
-            # dataset_id : {
-                # global_key : {
-                    # "data_row" : {}, -- This is your data row upload as a dictionary
-                    # "project_id" : "", -- This batches data rows to projects, if applicable
-                    # "annotations" : [] -- List of annotations for a given data row, if applicable
-                # }
+        if not create_action:
+            raise ValueError(f"No `dataset_id` argument or `dataset_id` column was provided in table to create data rows")
+        
+        # Create an upload dictionary where:
+        # {
+            # global_key : {
+                # "data_row" : {}, -- This is your data row upload as a dictionary
+                # "dataset_id" : "" -- This is the dataset a global_key should go to (or belongs to)
+                # "project_id" : "", -- This batches data rows to projects, if applicable
+                # "annotations" : [], -- List of annotations for a given data row, if applicable
+                # "model_run_id" : "", -- This adds data rows to model runs, if applicable
+                # "predictions" : [] -- List of predictions for a given data row, if applicable
             # }
         # }
         # This uniforms the upload to use labelbase - Labelbox base code for best practices
         upload_dict = create_upload_dict(
             client=self.lb_client, table=table, table_dict=table_dict, 
-            row_data_col=row_data_col, global_key_col=global_key_col, external_id_col=external_id_col, 
-            dataset_id_col=dataset_id_col, dataset_id=dataset_id, 
-            project_id_col=project_id_col, project_id=project_id,
-            metadata_index=metadata_index, attachment_index=attachment_index, annotation_index=annotation_index,
+            row_data_col=x["row_data_col"], global_key_col=x["global_key_col"], external_id_col=x["external_id_col"], 
+            dataset_id_col=x["dataset_id_col"], dataset_id=dataset_id, project_id_col=x["project_id_col"], project_id=project_id,
+            metadata_index=x["metadata_index"], attachment_index=x["attachment_index"], annotation_index=x["annotation_index"],
+            create_action=create_action, annotate_action=annotate_action, prediction_action=prediction_action,
             upload_method=upload_method, mask_method=mask_method, divider=divider, verbose=verbose
         )      
                 
@@ -131,22 +139,17 @@ class Client():
         if batch_action: 
             try:
                 # Create a dictionary where {key=global_key : value=data_row_id}
-                global_keys_list = []
-                for dataset_id in upload_dict.keys():
-                    for global_key in upload_dict[dataset_id].keys():
-                        global_keys_list.append(global_key)
                 global_key_to_data_row_id = create_global_key_to_data_row_dict(
-                    client=self.lb_client, global_keys=global_keys_list
+                    client=self.lb_client, global_keys=list(upload_dict.keys())
                 )     
                 # Create batch dictionary where {key=project_id : value=[data_row_ids]}   
                 project_id_to_batch_dict = {}                
-                for dataset_id in upload_dict:
-                    for global_key in upload_dict[dataset_id].keys():                    
-                        project_id = upload_dict[dataset_id][global_key]["project_id"]
-                        if project_id:
-                            if project_id not in project_id_to_batch_dict.keys():
-                                project_id_to_batch_dict[project_id] = []
-                            project_id_to_batch_dict[project_id].append(global_key_to_data_row_id[global_key])
+                for global_key in upload_dict.keys():                  
+                    project_id = upload_dict[global_key]["project_id"]
+                    if project_id:
+                        if project_id not in project_id_to_batch_dict.keys():
+                            project_id_to_batch_dict[project_id] = []
+                        project_id_to_batch_dict[project_id].append(global_key_to_data_row_id[global_key])
                 # Labelbase command to batch data rows to projects
                 batch_to_project_results = batch_rows_to_project(
                     client=self.lb_client, project_id_to_batch_dict=project_id_to_batch_dict, 
@@ -163,17 +166,17 @@ class Client():
             try:               
                 # Create batch dictionary where {key=project_id : value=[data_row_ids]}
                 project_id_to_upload_dict = {}
-                for dataset_id in upload_dict.keys():
-                    for global_key in upload_dict[dataset_id].keys():
-                        project_id = upload_dict[dataset_id][global_key]["project_id"]
-                        annotations_no_data_row_id = upload_dict[dataset_id][global_key]["annotations"]
+                for global_key in upload_dict.keys():
+                    project_id = upload_dict[global_key]["project_id"]
+                    annotations_no_data_row_id = upload_dict[global_key]["annotations"]
+                    if annotations_no_data_row_id: 
                         if project_id not in project_id_to_upload_dict.keys():
                             project_id_to_upload_dict[project_id] = []
-                        if annotations_no_data_row_id: # For each annotation in the list, add the data row ID and add it to your annotation upload dict
-                            for annotation in annotations_no_data_row_id:
-                                annotation_data_row_id = annotation
-                                annotation_data_row_id["dataRow"] = {"id" : global_key_to_data_row_id[global_key]}
-                                project_id_to_upload_dict[project_id].append(annotation_data_row_id)
+                        # Add data row ID to your annotations to-be-uploaded
+                        for annotation in annotations_no_data_row_id:
+                            annotation_data_row_id = annotation
+                            annotation_data_row_id["dataRow"] = {"id" : global_key_to_data_row_id[global_key]}
+                            project_id_to_upload_dict[project_id].append(annotation_data_row_id)
                 # Labelbase command to upload annotations to projects
                 annotation_upload_results = batch_upload_annotations(
                     client=self.lb_client, project_id_to_upload_dict=project_id_to_upload_dict, how=upload_method, verbose=verbose
@@ -181,12 +184,39 @@ class Client():
             except Exception as e:
                 annotation_upload_results = e
         else:
-            annotation_upload_results = []              
+            annotation_upload_results = []         
+            
+        # Annotation upload attempt
+        if prediction_action:
+            try:               
+                # Create batch dictionary where {key=project_id : value=[data_row_ids]}
+                model_run_id_to_upload_dict = {}
+                for global_key in upload_dict.keys():
+                    model_run_id = upload_dict[global_key]["model_run_id"]
+                    predictions_no_data_row_id = upload_dict[global_key]["predictions"]
+                    if annotations_no_data_row_id:
+                        if model_run_id not in model_run_id_to_upload_dict.keys():
+                            model_run_id_to_upload_dict[model_run_id] = []
+                        if annotate_action:
+                            project_id_to_upload_dict[model_run_id].extend(annotations)
+                        for preidction in predictions_no_data_row_id:
+                            prediction_data_row_id = annotation
+                            prediction_data_row_id["dataRow"] = {"id" : global_key_to_data_row_id[global_key]}
+                            model_run_id_to_upload_dict[model_run_id].append(prediction_data_row_id)
+                # Labelbase command to upload annotations to projects
+                annotation_upload_results = batch_upload_predictions(
+                    client=self.lb_client, model_run_id_to_upload_dict=model_run_id_to_upload_dict, how=upload_method, verbose=verbose
+                )
+            except Exception as e:
+                prediction_upload_results = e
+        else:
+            prediction_upload_results = []               
             
         return {
             "data_row_upload_results" : data_row_upload_results, 
             "batch_to_project_results" : batch_to_project_results,
-            "annotation_upload_results" : annotation_upload_results
+            "annotation_upload_results" : annotation_upload_results,
+            "prediction_upload_results" : prediction_upload_results
         }
     
     # def upsert_table_metadata():
