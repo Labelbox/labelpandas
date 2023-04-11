e7fac94398fdfbca68b2ba86c180d698aae85382
raphaeljafriLB <79109817+raphaeljafriLB@users.noreply.github.com>
2023-04-12 00:28:50 +0200

    Update client.py

--- labelpandas/client.py
+++ labelpandas/client.py
@@ -155,9 +155,10 @@ class Client():
         
         # If performing actions that require data row IDs, we pull them here using labelbase.uploader.create_global_key_to_data_row_id_dict
         if actions["batch"] or actions["annotate"] or actions["prediction"]:
-            print(upload_dict.keys())
+            all_global_keys = list(upload_dict.keys())
+            print(all_global_keys)
             global_key_to_data_row_id = create_global_key_to_data_row_id_dict(
-                client=self.lb_client, global_keys=list(upload_dict.keys())
+                client=self.lb_client, global_keys=all_global_keys
             )
         
         # Bath to project attempt using labelbase.uploader.batch_rows_to_project
